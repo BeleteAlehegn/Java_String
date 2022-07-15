a088b674af5145911a659e66abea57a528be3d0d
unknown <hilegiorgisbelete@gmail.com>
2022-07-15 10:45:21 -0700

    first commit

---
+++ concatination.java
@@ -0,0 +1,17 @@
+public class concatination {
+
+    public static void main(String[] args) {
+        
+        String first="My natinality is ";
+        String last="Ethiopian";
+        //concatination 
+        System.out.println(first.concat(last));
+        //length of string
+        System.out.println("The length of the word "+ last +" is: "+ last.length());
+        //index of the first existance
+        System.out.println("The index of a in the word "+last+ " is: "+ last.indexOf("a"));
+
+        return;
+    }
+
+}
