f1b22289cbd242c115eab3ef7dfa0caeaa3a9bed
unknown <hilegiorgisbelete@gmail.com>
2022-07-15 11:43:51 -0700

    second commit

---
+++ _java_math_methods.java
@@ -0,0 +1,13 @@
+public class _java_math_methods{
+    public static void main(String[] args) {
+        int x=20,y=8;
+        //identifying maximum number
+        System.out.println("The largest number from "+x+ " and "+y+ " is: "+ Math.max(x,y));
+         //identifying Minimum number
+         System.out.println("The smallest number from "+x+ " and "+y+ " is: "+ Math.min(x,y));
+          //To generate random number from 0 up tp 10
+        System.out.println("The random number is: "+(int)(Math.random()*11));
+
+        
+    }
+}
